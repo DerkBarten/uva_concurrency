ebec9c6c7af7bebe0ad19daea8e0dc4ff035acb3
DerkBarten <derk.barten@gmail.com>
2017-11-07 13:21:20 +0100

    Progress commit

--- assignment1/assign_1_2_framework/assign_1_2_framework/simulate.c
+++ assignment1/assign_1_2_framework/assign_1_2_framework/simulate.c
@@ -9,6 +9,19 @@
 
 #include "simulate.h"
 
+double WAVE_C = 0.15; 
+
+/* Add any functions you may need (like a worker) here. */
+double wave(double *old_array, double *current_array, int i, int i_max) {
+    // Both endpoints are always zero
+    if (i == 0 || i == i_max - 1){
+        return 0;
+    }
+    // Can threads read same memory address at the same time?
+    return 2 * current_array[i] - old_array[i] + 
+           WAVE_C * (current_array[i - 1] - 
+           (2 * current_array[i] - current_array[i + 1]));
+}
 
 /*
  * Executes the entire simulation.
@@ -28,6 +41,11 @@ double *simulate(const int i_max, const int t_max, const int num_threads,
     /*
      * Your implementation should go here.
      */
+    for (int i = 0; i < t_max; i++) {
+        for (int j = 0; j < i_max; j++){
+            next_array[j] = wave(old_array, current_array, j, i_max);
+        }
+    }
 
     return current_array;
 }
