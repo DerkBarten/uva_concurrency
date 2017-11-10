2d48bbec6180118f64591ff066325be1765c9254
Giovkast <giovkast@gmail.com>
2017-11-10 17:12:12 +0100

    added code for implementation

---
+++ assignment1/assign_1_2_framework/sieves/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <pthread.h>
+
+int main() {
+
+int N = 30;
+int p = 2;
+int sieve[N];
+
+void generate_numbers(list* generator, int start_value) {
+
+    while(true) {
+         // make new list entry
+          // make new list entry.next = list.start
+          // make new list entry
+    }
+    // make new list entry
+    // push a new value on the list
+}
