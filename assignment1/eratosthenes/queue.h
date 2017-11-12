a54138c91d24f7bdc021f1181989a0c89e71c6d4
DerkBarten <derk.barten@gmail.com>
2017-11-12 11:53:10 +0100

    Finished Queue

---
+++ assignment1/eratosthenes/queue.h
@@ -0,0 +1,16 @@
+struct queue {
+    struct queue_entry *first;
+    struct queue_entry *last;
+};
+
+struct queue_entry {
+    int value;
+    struct queue_entry *next;
+    struct queue_entry *previous;
+};
+
+int push(struct queue *queue, int value);
+
+int pop(struct queue *queue);
+
+int print_queue(struct queue *queue);
