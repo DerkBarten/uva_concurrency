bb3d571ef50eb2b5c1e9a1fc72c21fb4e9a934f0
DerkBarten <derk.barten@gmail.com>
2017-11-17 13:44:33 +0100

    Progress commit

--- assignment2/simulate.c
+++ assignment2/simulate.c
@@ -58,21 +58,20 @@ double *simulate(const int i_max, const int t_max, double *old_array,
 
     int left_neighbor_rank = rank - 1 ;
     int right_neighbor_rank = rank + 1;
-
     int array_length;
+    int array_leftover;
+
     // If there is just one task
     if (numtasks == 1) {
         array_length = i_max;
-    }
-    // If the task is the last one
-    else if (right_neighbor_rank >= numtasks) {
-        
-        array_length = i_max % (numtasks - 1);
     }
     else{
         array_length = i_max / (numtasks - 1);
     }
 
+    array_leftover = i_max % (numtasks - 1);
+
+    // Calculate where the arrays
     my_old_array = old_array + rank * array_length;
     my_current_array = current_array + rank * array_length;
     my_next_array = next_array + rank * array_length;
@@ -86,9 +85,10 @@ double *simulate(const int i_max, const int t_max, double *old_array,
     // When there is just one worker
     if (numtasks == 1) {
         for (int t = 0; t < t_max; t++) {
-            for (int i = 0; i < array_length - 1; i++) {
-                next_array[i] = wave(i);
+            for (int i = 0; i < array_length; i++) {
+                my_next_array[i] = wave(i);
             }
+            buffer_swap();
         }
     }
     // The chunks that are not at the edges
@@ -97,8 +97,8 @@ double *simulate(const int i_max, const int t_max, double *old_array,
             MPI_Isend(&my_left, 1, MPI_FLOAT, left_neighbor_rank, 1, MPI_COMM_WORLD, &reqs[0]);
             MPI_Isend(&my_right, 1, MPI_FLOAT, right_neighbor_rank, 2, MPI_COMM_WORLD, &reqs[1]);
 
-            for (int i = 1; i < array_length - 2; i++) {
-                next_array[i] = wave(i);
+            for (int i = 1; i < array_length - 1; i++) {
+                my_next_array[i] = wave(i);
             }
 
             MPI_Recv(&left_neighbor_float, 1, MPI_FLOAT, left_neighbor_rank, 2, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
@@ -107,30 +107,30 @@ double *simulate(const int i_max, const int t_max, double *old_array,
             my_current_array[0] = left_neighbor_float;
             my_current_array[array_length - 1] = right_neighbor_float;
 
-            next_array[0] = wave(0);
-            next_array[array_length - 1] = wave(array_length - 1);
+            my_next_array[0] = wave(0);
+            my_next_array[array_length - 1] = wave(array_length - 1);
 
             buffer_swap();
         }
         
     }
-    // The rightmost chunk
+    // The right most chunk
     else if (right_neighbor_rank >= numtasks) {
         for (int t = 0; t < t_max; t++) {
 
             MPI_Isend(&my_left, 1, MPI_FLOAT, left_neighbor_rank, 1, MPI_COMM_WORLD, &reqs[0]);
             
-            for (int i = 1; i < array_length - 2; i++) {
-                next_array[i] = wave(i);
+            for (int i = 1; i < array_leftover - 1; i++) {
+                my_next_array[i] = wave(i);
             }
 
             MPI_Recv(&left_neighbor_float, 1, MPI_FLOAT, left_neighbor_rank, 2, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
-            my_current_array[array_length - 1] = 0.0;
+            my_current_array[array_leftover - 1] = 0.0;
             my_current_array[0] = left_neighbor_float;
 
-            next_array[0] = wave(0);
-            next_array[array_length - 1] = 0.0;
+            my_next_array[0] = wave(0);
+            my_next_array[array_leftover - 1] = 0.0;
         }         
     }
     // The left most chunk
@@ -138,8 +138,8 @@ double *simulate(const int i_max, const int t_max, double *old_array,
         for (int t = 0; t < t_max; t++) {
             MPI_Isend(&my_right, 1, MPI_FLOAT, right_neighbor_rank, 2, MPI_COMM_WORLD, &reqs[0]);
             
-            for (int i = 1; i < array_length - 2; i++) {
-                next_array[i] = wave(i);
+            for (int i = 1; i < array_length - 1; i++) {
+                my_next_array[i] = wave(i);
             }
 
             MPI_Recv(&right_neighbor_float, 1, MPI_FLOAT, right_neighbor_rank, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
@@ -147,13 +147,13 @@ double *simulate(const int i_max, const int t_max, double *old_array,
             my_current_array[0] = 0.0;
             my_current_array[array_length - 1] = right_neighbor_float;
             
-            next_array[0] = 0.0;
-            next_array[array_length - 1] = wave(array_length - 1);
+            my_next_array[0] = 0.0;
+            my_next_array[array_length - 1] = wave(array_length - 1);
         }
 
     }
     
     MPI_Finalize();
-    /* You should return a pointer to the array with the final results. */
-    return current_array;
+    printf("Proceces %i finished\n", rank);
+    return my_current_array;
 }
