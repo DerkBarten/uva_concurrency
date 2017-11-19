7a544a9b8928b0ff29e0a45cb0629c042ea69de9
Giovkast <giovanni.kastanja@student.uva.nl>
2017-11-19 18:59:42 +0100

    built the basic broadcast, but it's inefficient

--- assignment2/broadcast.c
+++ assignment2/broadcast.c
@@ -8,7 +8,15 @@ int MYMPI_Bcast (void *buffer, int count, MPI_Datatype datatype, int root, MPI_C
     //MPI_Request reqs[numtasks];    
 
     if (rank == root) {
-        MPI_Isend(buffer, count, datatype, i, 0, communicator, &reqs[i]);
+        int i;
+        // if the rank is the root then send to all other processes
+        for( i = 0; i < numtasks; i++) {
+            if( i != rank) {
+                MPI_send(buffer, count, datatype, i, 0, communicator);
+            }
+        }
+    } else {
+        MPI_recv(buffer, count, datatype, root, 0, communicator, MPI_Status_Ignore);
     }
     return 0;
 }
