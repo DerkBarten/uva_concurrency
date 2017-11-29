56e86ddc66894504e3c491d019c423fdf37fc4c5
DerkBarten <derkbarten@gmail.com>
2017-11-29 10:34:07 +0100

    Working Retweet reply count

---
+++ assignment3/src/main/java/nl/uva/cpp/RetweetReplyCount/RetweetReplyCountReducer.java
@@ -0,0 +1,21 @@
+package nl.uva.cpp.RetweetReplyCount;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapreduce.*;
+
+public class RetweetReplyCountReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+
+	@Override
+	public void reduce(Text key, Iterable<IntWritable> values, Context context) 
+			throws IOException, InterruptedException {
+		int sum = 0;
+		int count = 0;
+		for (IntWritable val : values) {
+			sum += val.get();
+			count++;
+		}
+		context.write(key, new IntWritable(sum));
+	}
+}
