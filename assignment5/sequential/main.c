e539f4c5fad6a9aa8e3e6204d55245049622be88
DerkBarten <derkbarten@gmail.com>
2017-12-19 19:14:01 +0100

    Progress commit

--- assignment5/sequential/main.c
+++ assignment5/sequential/main.c
@@ -8,9 +8,9 @@ int main(int argc, char *argv[]) {
         image_t output;
 
         load_image(argv[1], &input);
-        rgb_to_grayscale(&input, &output);
-        contrast_modification(&output);
-        triangular_smoothing(&output); 
+        grayscale(&input, &output);
+        contrast(&output);
+        smoothing(&output); 
         save_image(argv[2], &output);
 
         // Free the data array in the image object
