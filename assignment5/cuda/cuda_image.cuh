2eca8e1a429b284e3ff780fb9d3a29c4a5929292
DerkBarten <derkbarten@gmail.com>
2017-12-19 10:04:23 +0100

    patch

--- assignment5/cuda/cuda_image.cuh
+++ assignment5/cuda/cuda_image.cuh
@@ -5,3 +5,5 @@ void cuda_grayscale(image_t *input, image_t *output);
 void cuda_contrast(image_t *image);
 
 void cuda_smoothing(image_t *image);
+
+void cuda_image(image_t *input, image_t *output);
